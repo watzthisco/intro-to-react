5ab289ebb3e29a8d39f8f8a537244665278bf6c4
chrisminnick <chris@watzthis.com>
2018-02-26 14:24:01 -0800

    finishing lab06

--- solutions/lab06/src/ProductList.js
+++ solutions/lab06/src/ProductList.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Product from './Product';
+import styles from './ProductList.css.js';
 
 class ProductList extends React.Component{
     render(){
         return(
-            <div>
-                <Product />
-                <Product />
-            </div>
+            <ul style={styles.productList}>
+                <li style={styles.productListItem}><Product /></li>
+                <li style={styles.productListItem}><Product /></li>
+                <li style={styles.productListItem}><Product /></li>
+                <li style={styles.productListItem}><Product /></li>
+                <li style={styles.productListItem}><Product /></li>
+                <li style={styles.productListItem}><Product /></li>
+                <li style={styles.productListItem}><Product /></li>
+                <li style={styles.productListItem}><Product /></li>
+                <li style={styles.productListItem}><Product /></li>
+                <li style={styles.productListItem}><Product /></li>
+            </ul>
         );
     }
 }
