e3b359868fbe6f596b95b68de73e6c6f362fe34e
chrisminnick <chris@watzthis.com>
2018-03-08 09:49:24 -0800

    added sorting in componentWillMount

---
+++ solutions/lab10/src/components/Cart.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import CartItem from './CartItem';
+import styles from './Cart.css.js';
+
+class Cart extends React.Component{
+
+    calculateTotal(items){
+        let total = 0;
+        for (let i = 0; i<items.length; i++) {
+            total += Number(items[i].price);
+        }
+        return total;
+    }
+    render(){
+        return(
+            <div style={styles.cart}>
+                <h2>Cart</h2>
+                {this.props.inCart.map(item=>(
+                    <CartItem key={item.id} {...item} />
+                ))}
+                Total: ${this.calculateTotal(this.props.inCart)} USD
+            </div>
+        );
+    }
+}
+
+export default Cart;
