840c832b8adf1ff59b311332d9aa06fc1a93a9f0
chrisminnick <chris@watzthis.com>
2018-03-08 11:25:46 -0800

    started writing lab12

--- solutions/lab12/src/components/CartItem.test.js
+++ solutions/lab12/src/components/CartItem.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CartItem from './CartItem';
+import {shallow} from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CartItem />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const renderedCartItem = shallow(<CartItem />);
 });
