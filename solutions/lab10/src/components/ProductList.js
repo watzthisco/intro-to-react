f81034d6a0fe12f9fc9f2fa98674a6f9844d4d04
chrisminnick <chris@watzthis.com>
2018-05-07 16:42:11 -0700

    changing lab 10 to load data in componentdidmount

--- solutions/lab10/src/components/ProductList.js
+++ solutions/lab10/src/components/ProductList.js
@@ -5,19 +5,25 @@ import styles from './ProductList.css.js';
 class ProductList extends React.Component{
     render(){
         const inCart = this.props.inCart;
-        return(
-            <ul style={styles.productList}>
-                {this.props.products.map(product => (
-                    <li key={product.id} style={styles.productListItem}>
-                        <Product {...product}
-                                 inCart = {inCart.includes(product.id)?'1':''}
-                                 addToCart={this.props.addToCart}
-                                 removeFromCart={this.props.removeFromCart}
-                        />
-                    </li>
-                ))}
-            </ul>
-        );
+        const products = this.props.products;
+
+        if(products.length > 0) {
+            return (
+                <ul style={styles.productList}>
+                    {products.map(product => (
+                        <li key={product.id} style={styles.productListItem}>
+                            <Product {...product}
+                                     inCart={inCart.includes(product.id) ? '1' : ''}
+                                     addToCart={this.props.addToCart}
+                                     removeFromCart={this.props.removeFromCart}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            );
+        } else {
+            return null;
+        }
     }
 }
 
